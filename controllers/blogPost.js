93240f54237c1a981374b82f7dd9f3cd12f291c2
Helena <helena@klarr.se>
2024-01-16 10:10:34 +0100

    started on update

--- controllers/blogPost.js
+++ controllers/blogPost.js
@@ -18,3 +18,31 @@ exports.getAll = async (req, res) => {
   const allBlogPosts = await BlogPost.find({}).sort({ createdAt: 1 }).exec();
   res.json(allBlogPosts);
 };
+
+exports.update = async (req, res) => {
+  const blogPost = req.blogPost;
+  blogPost.title = req.body.title;
+  blogPost.content = req.body.content;
+  blogPost.save((err, data) => {
+    if (err) {
+      return res.status(400).json({ message: "Error updating blog post" });
+    }
+    res.json(data);
+  });
+};
+
+// PARAM
+exports.blogPostById = async (req, res) => {
+  BlogPost.findById(id).exec((err, data) => {
+    if (err || !data) {
+      return res.status(400).json({ message: "Error" });
+    }
+    req.data = data;
+    next();
+  });
+};
+
+//GET blogpost by specific id
+exports.read = async (req, res) => {
+  return res.json(req.blogPost);
+};
--- routes/blogPost.js
+++ routes/blogPost.js
@@ -2,12 +2,17 @@ const express = require("express");
 const router = express.Router();
 
 // controller methods
-const { create, getAll } = require("../controllers/blogPost");
+const { create, getAll, update } = require("../controllers/blogPost");
 
 // POST create post
 router.post("/blogpost", create);
 
 // GET get all posts
 router.get("/blogposts", getAll);
+
+// PUT update blog post
+router.put("/blogpost/:postId", update);
+
+// GET specific blogpost based on id
 
 module.exports = router;
